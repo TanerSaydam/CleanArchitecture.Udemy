1457f2106a93e3e8cad092771e458555680d479c
tanersaydam <tanersaydam@gmail.com>
2023-04-27 00:43:06 +0300

    Ders 9: ApiController, CarsController

---
+++ CleanArchitecture.Presentation/Abstraction/ApiController.cs
@@ -0,0 +1,16 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CleanArchitecture.Presentation.Abstraction;
+
+[ApiController]
+[Route("api/[controller]")]
+public abstract class ApiController : ControllerBase
+{
+    public readonly IMediator _mediator;
+
+    protected ApiController(IMediator mediator)
+    {
+        _mediator = mediator;
+    }
+}
---
+++ CleanArchitecture.Presentation/Controllers/CarsController.cs
@@ -0,0 +1,19 @@
+﻿using CleanArchitecture.Application.Features.CarFeatures.Commands.CreateCar;
+using CleanArchitecture.Domain.Dtos;
+using CleanArchitecture.Presentation.Abstraction;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CleanArchitecture.Presentation.Controllers;
+
+public sealed class CarsController : ApiController
+{
+    public CarsController(IMediator mediator) : base(mediator) {}
+
+    [HttpPost("[action]")]
+    public async Task<IActionResult> Create(CreateCarCommand request, CancellationToken cancellationToken)
+    {
+        MessageResponse response =  await _mediator.Send(request, cancellationToken);
+        return Ok(response);
+    }
+}
