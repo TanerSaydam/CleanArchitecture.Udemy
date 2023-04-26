9feffda99227aebc34d6e97772ea439236301496
tanersaydam <tanersaydam@gmail.com>
2023-04-27 01:19:17 +0300

    Ders11: Fluent Validation, Behavior, IPipelineBehavior,ValidationBehavior

---
+++ CleanArchitecture.Application/Features/CarFeatures/Commands/CreateCar/CreateCarCommandValidator.cs
@@ -0,0 +1,19 @@
+﻿using FluentValidation;
+
+namespace CleanArchitecture.Application.Features.CarFeatures.Commands.CreateCar;
+
+public sealed class CreateCarCommandValidator : AbstractValidator<CreateCarCommand>
+{
+    public CreateCarCommandValidator()
+    {
+        RuleFor(p=> p.Name).NotEmpty().WithMessage("Araç adı boş olamaz!");
+        RuleFor(p=> p.Name).NotNull().WithMessage("Araç adı boş olamaz!");
+        RuleFor(p=> p.Name).MinimumLength(3).WithMessage("Araç adı en az 3 karakter olmalıdır!");
+        RuleFor(p => p.Model).NotEmpty().WithMessage("Araç modeli boş olamaz!");
+        RuleFor(p => p.Model).NotNull().WithMessage("Araç modeli boş olamaz!");
+        RuleFor(p => p.Model).MinimumLength(3).WithMessage("Araç modeli en az 3 karakter olmalıdır!");
+        RuleFor(p => p.EnginePower).NotEmpty().WithMessage("Araç motor gücü boş olamaz!");
+        RuleFor(p => p.EnginePower).NotNull().WithMessage("Araç motor gücü boş olamaz!");
+        RuleFor(p => p.EnginePower).GreaterThan(0).WithMessage("Araç motor gücü 0'dan büyük olmalıdır!");
+    }
+}
--- CleanArchitecture.WebApi/Program.cs
+++ CleanArchitecture.WebApi/Program.cs
@@ -1,6 +1,9 @@
+using CleanArchitecture.Application.Behaviors;
 using CleanArchitecture.Application.Services;
 using CleanArchitecture.Persistance.Context;
 using CleanArchitecture.Persistance.Services;
+using FluentValidation;
+using MediatR;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -19,6 +22,10 @@ builder.Services.AddControllers()
 
 builder.Services.AddMediatR(cfr =>
     cfr.RegisterServicesFromAssembly(typeof(CleanArchitecture.Application.AssemblyReference).Assembly));
+
+builder.Services.AddTransient(typeof(IPipelineBehavior<,>), typeof(ValidationBehavior<,>));
+
+builder.Services.AddValidatorsFromAssembly(typeof(CleanArchitecture.Application.AssemblyReference).Assembly);
 
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
