02075ecaf4a665f66cddb5b8b3e13842ec6c91d0
tanersaydam <tanersaydam@gmail.com>
2023-04-28 20:16:35 +0300

    Ders 21: Create Token Metodunu Yazalım

---
+++ CleanArchitecture.Application/Abstractions/IJwtProvider.cs
@@ -0,0 +1,8 @@
+﻿using CleanArchitecture.Domain.Entities;
+
+namespace CleanArchitecture.Application.Abstractions;
+
+public interface IJwtProvider
+{
+    string CreateToken(User user);
+}
---
+++ CleanArcihtecture.Infrastructure/Authenticaton/JwtProvider.cs
@@ -0,0 +1,41 @@
+﻿using CleanArchitecture.Application.Abstractions;
+using CleanArchitecture.Domain.Entities;
+using Microsoft.Extensions.Options;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+
+namespace CleanArcihtecture.Infrastructure.Authenticaton;
+
+public sealed class JwtProvider : IJwtProvider
+{
+    private readonly JwtOptions _jwtOptions;
+
+    public JwtProvider(IOptions<JwtOptions> jwtOptions)
+    {
+        _jwtOptions = jwtOptions.Value;
+    }
+
+    public string CreateToken(User user)
+    {
+        var claims = new Claim[]
+        {
+            new Claim(ClaimTypes.Email, user.Email),
+            new Claim(JwtRegisteredClaimNames.Name, user.UserName),
+            new Claim("NameLastName",user.NameLastName)
+        };
+
+        JwtSecurityToken jwtSecurityToken = new(
+            issuer: _jwtOptions.Issuer,
+            audience: _jwtOptions.Audience,
+            claims: claims,
+            notBefore: DateTime.Now,
+            expires: DateTime.Now.AddHours(1),
+            signingCredentials: new SigningCredentials(new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_jwtOptions.SecretKey)),SecurityAlgorithms.HmacSha256));
+
+        string token = new JwtSecurityTokenHandler().WriteToken(jwtSecurityToken);
+
+        return token;
+    }
+}
